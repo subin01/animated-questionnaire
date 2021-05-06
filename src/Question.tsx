23dd34474979cf6eb7135bf8bd1262ea6e93b634
Subin Paul <subin01@gmail.com>
2021-05-06 15:26:28 +1000

    Keyboard shortcuts, NSW theme

--- src/Question.tsx
+++ src/Question.tsx
@@ -1,5 +1,7 @@
 // @ts-nocheck
+import { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
+import { useHotkeys } from 'react-hotkeys-hook'
 
 const spring = {
   type: 'spring',
@@ -27,18 +29,30 @@ const variant = {
 
 interface Props {
   item: {
-    qid: string;
-    type: string;
-    question: string;
-    answers: [string];
-    chosenAnswer?: string;
-    button?: string;
-  };
-  nextSlide: () => {};
-  chooseAnswer: () => {};
+    qid: string
+    type: string
+    question: string
+    answers: [string]
+    chosenAnswer?: string
+    button?: string
+  }
+  nextSlide: () => {}
+  chooseAnswer: () => {}
 }
 
 export function Options({ qid, answers, chosenAnswer, chooseAnswer }) {
+  useHotkeys('1', () => {
+    chooseAnswer(qid, answers && answers[0])
+  })
+  useHotkeys('2', () => {
+    chooseAnswer(qid, answers && answers[1])
+  })
+  useHotkeys('3', () => {
+    chooseAnswer(qid, answers && answers[2])
+  })
+  useHotkeys('4', () => {
+    chooseAnswer(qid, answers && answers[3])
+  })
   return answers.map((ans, i) => (
     <motion.li
       key={i}
@@ -46,13 +60,29 @@ export function Options({ qid, answers, chosenAnswer, chooseAnswer }) {
       onClick={() => chooseAnswer(qid, ans)}
       className={`${chosenAnswer === ans ? 'active' : ''}`}
     >
+      <kbd title={`Key ${i + 1}`}>{i + 1}</kbd>
       <a href="#">{ans}</a>
     </motion.li>
   ))
 }
 
 export default function Question({ item, chooseAnswer, nextSlide }: Props) {
-  const isEnableNext = item.chosenAnswer || item.type === 'welcome'
+  // const [answer, setAnswer] = useState(item.chosenAnswer)
+
+  const isWelcomeSlide = item.type === 'welcome'
+  function isEnableNext() {
+    const hasChosenAnswer = typeof item.chosenAnswer !== 'undefined'
+    return hasChosenAnswer || isWelcomeSlide
+  }
+
+  const handleNextSlide = () => {
+    if (isEnableNext()) {
+      nextSlide()
+    }
+  }
+
+  useHotkeys('enter', handleNextSlide)
+
   return (
     <motion.article
       variants={questionVariants}
@@ -75,11 +105,21 @@ export default function Question({ item, chooseAnswer, nextSlide }: Props) {
         </ol>
       )}
       <motion.div variants={item} className="button-container">
-        <button onClick={nextSlide} disabled={!isEnableNext}>
-          {item.button} &rarr;
+        <button onClick={handleNextSlide} disabled={!isEnableNext()}>
+          {item.button}
         </button>
-        {!isEnableNext && <span className="msg">Please choose one to continue</span>}
+        <kbd title="Press Enter/Return key">Enter ↵</kbd>
+        <div className="msg">{!isEnableNext() && 'Please choose an answer to continue'}</div>
       </motion.div>
+      {isWelcomeSlide && (
+        <motion.div
+          className="tips"
+          animate={{ opacity: [0, 1], y: [10, 0] }}
+          transition={{ duration: 0.5, delay: 2 }}
+        >
+          <strong>&#9432; Tip:</strong> Keyboard shortcuts are marked with <kbd>Key</kbd>
+        </motion.div>
+      )}
     </motion.article>
   )
 }
